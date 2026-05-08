8db0c8853c5ef37c77d67eb12c752f35d17f5f95
Jprimero15 <jprimero15>
2026-05-09 04:48:21 +0800

    fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,6 +13,11 @@ android {
         targetSdk = 34
     }
 
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
     buildFeatures { compose = true }
 
     composeOptions {
