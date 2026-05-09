2811982f471434f2eb5efb71262550543d05f9d3
Jprimero15 <jprimero15>
2026-05-09 15:58:00 +0800

    fixx

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,33 +1,35 @@
-// app/build.gradle
 plugins {
-    id 'com.android.application'
-    id 'kotlin-android'
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
 }
 
 android {
-    compileSdk 34
+    namespace = "com.new.flappybird"
+    compileSdk = 34
 
     defaultConfig {
-        applicationId "com.new.flappybird"
-        minSdk 21
-        targetSdk 34
-        versionCode 1
-        versionName "1.0"
+        applicationId = "com.new.flappybird"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
     }
 
-    buildTypes {
-        release {
-            minifyEnabled false
-        }
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.8"
     }
 
     compileOptions {
-        sourceCompatibility JavaVersion.VERSION_1_8
-        targetCompatibility JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = '1.8'
+        jvmTarget = "17"
     }
 }
 
