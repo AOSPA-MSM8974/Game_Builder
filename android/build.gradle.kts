df9425c0565e924443404792de9ae44421931c24
Jprimero15 <jprimero15>
2026-05-11 19:32:32 +0800

    grqdle again damn

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -6,42 +6,33 @@ plugins {
 android {
 
     namespace = "com.stickrun.game"
-
     compileSdk = 34
 
     defaultConfig {
-
         applicationId = "com.stickrun.game"
-
         minSdk = 26
         targetSdk = 34
-
         versionCode = 1
         versionName = "1.0"
     }
 
     buildTypes {
-
         release {
             isMinifyEnabled = false
         }
     }
 
     compileOptions {
-
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
 
     kotlinOptions {
-
         jvmTarget = "1.8"
     }
 
     packaging {
-
         jniLibs {
-
             useLegacyPackaging = true
         }
     }
@@ -51,19 +42,18 @@ dependencies {
 
     implementation(project(":core"))
 
-    implementation(libs.kotlin.stdlib)
+    implementation(libs.gdx.core)
+    implementation(libs.gdx.backend.android)
 
-    implementation("com.badlogicgames.gdx:gdx-backend-android:1.12.1")
+    implementation("com.badlogicgames.gdx:gdx-platform:${libs.versions.gdx.get()}:natives-armeabi-v7a")
+    implementation("com.badlogicgames.gdx:gdx-platform:${libs.versions.gdx.get()}:natives-arm64-v8a")
+    implementation("com.badlogicgames.gdx:gdx-platform:${libs.versions.gdx.get()}:natives-x86")
+    implementation("com.badlogicgames.gdx:gdx-platform:${libs.versions.gdx.get()}:natives-x86_64")
 
-    implementation("com.badlogicgames.gdx:gdx-platform:1.12.1:natives-armeabi-v7a")
-    implementation("com.badlogicgames.gdx:gdx-platform:1.12.1:natives-arm64-v8a")
-    implementation("com.badlogicgames.gdx:gdx-platform:1.12.1:natives-x86")
-    implementation("com.badlogicgames.gdx:gdx-platform:1.12.1:natives-x86_64")
+    implementation("com.badlogicgames.gdx:gdx-box2d:${libs.versions.gdx.get()}")
 
-    implementation("com.badlogicgames.gdx:gdx-box2d:1.12.1")
-
-    implementation("com.badlogicgames.gdx:gdx-box2d-platform:1.12.1:natives-armeabi-v7a")
-    implementation("com.badlogicgames.gdx:gdx-box2d-platform:1.12.1:natives-arm64-v8a")
-    implementation("com.badlogicgames.gdx:gdx-box2d-platform:1.12.1:natives-x86")
-    implementation("com.badlogicgames.gdx:gdx-box2d-platform:1.12.1:natives-x86_64")
+    implementation("com.badlogicgames.gdx:gdx-box2d-platform:${libs.versions.gdx.get()}:natives-armeabi-v7a")
+    implementation("com.badlogicgames.gdx:gdx-box2d-platform:${libs.versions.gdx.get()}:natives-arm64-v8a")
+    implementation("com.badlogicgames.gdx:gdx-box2d-platform:${libs.versions.gdx.get()}:natives-x86")
+    implementation("com.badlogicgames.gdx:gdx-box2d-platform:${libs.versions.gdx.get()}:natives-x86_64")
 }
